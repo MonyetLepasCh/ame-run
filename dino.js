82a58463ab29b585940252160470870d335f74b9
joykeerz <wisnu.mukti10@gmail.com>
2023-03-11 09:29:06 +0700

    png update

--- dino.js
+++ dino.js
@@ -36,18 +36,18 @@ export function getDinoRect() {
 }
 
 export function setDinoLose() {
-  dinoElem.src = "imgs/loose.png"
+  dinoElem.src = "imgs/loosee.png"
 }
 
 function handleRun(delta, speedScale) {
   if (isJumping) {
-    dinoElem.src = `imgs/b2.png`
+    dinoElem.src = `imgs/b-2.png`
     return
   }
 
   if (currentFrameTime >= FRAME_TIME) {
     dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT
-    dinoElem.src = `imgs/b-${dinoFrame}.png`
+    dinoElem.src = `imgs/b${dinoFrame}.png`
     currentFrameTime -= FRAME_TIME
   }
   currentFrameTime += delta * speedScale
